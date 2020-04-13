105f791c1e7bc079f8ed6fd0a2f9a2cf2a76b4a6
yanwei <yanwei@playcrab.com>
2020-04-14 01:30:09 +0800

    up

--- main.lua
+++ main.lua
@@ -6,7 +6,11 @@ local trangles = {}
 
 local adjacent = {}
 
-local alpha = 80
+local smoothVerts = {}
+
+local weights = {}
+
+local alpha = 50
 
 local function PMinus(p2, p1)
     local p = {}
@@ -14,6 +18,40 @@ local function PMinus(p2, p1)
     p.y = p2.y - p1.y
     p.z = p2.z - p1.z
     return p
+end
+
+local function PAdd(p1, p2)
+    local p = {}
+    p.x = p2.x + p1.x
+    p.y = p2.y + p1.y
+    p.z = p2.z + p1.z
+    return p
+end
+
+local function PDevide(p1, devide)
+    p1.x = p1.x / devide
+    p1.y = p1.y / devide
+    p1.y = p1.y / devide
+end
+
+local function PNormalize(p, isreferece)
+    local len2 = p.x * p.x + p.y * p.y + p.z * p.z
+    if len2 > 0 then
+        local len = math.sqrt(len2)
+        if isreferece then
+            p.x = p.x / len
+            p.y = p.y / len
+            p.z = p.z / len
+            return
+        end
+
+        local ret = {}
+        ret.x = p.x / len
+        ret.y = p.y / len
+        ret.z = p.z / len
+        return ret
+    end
+    assert(false)
 end
 
 local function CalcNormal(face)
@@ -36,11 +74,10 @@ local function CalcNormal(face)
         n2 = n2 / len
         return {x = n0, y = n1, z = n2}
     end
-    local a
     assert(false)
 end
 
-for line in io.lines("/Users/bytedance/Desktop/ProjectX/model.obj") do
+for line in io.lines("/Users/yanwei/Desktop/ProjectX/model.obj") do
     local input = {}
     for splits in string.gmatch(line, reg) do
         input[#input + 1] = splits
@@ -66,6 +103,8 @@ for line in io.lines("/Users/bytedance/Desktop/ProjectX/model.obj") do
             vdx3 = v[3]
         }
 
+        weights[len + 1] = 1
+
         table.sort(
             v,
             function(a, b)
@@ -90,6 +129,9 @@ for line in io.lines("/Users/bytedance/Desktop/ProjectX/model.obj") do
 end
 
 local cases = 0
+
+local needSmoothFace = {}
+
 for id, faceIndexes in pairs(adjacent) do
     assert(#faceIndexes == 2)
     local normal1 = CalcNormal(trangles[faceIndexes[1]])
@@ -102,28 +144,82 @@ for id, faceIndexes in pairs(adjacent) do
     local cos = dot / (math.sqrt(dis1) * math.sqrt(dis2))
     local b = math.acos(cos)
     local an = math.acos(cos) * 180 / math.pi
-    if alpha > 80 then
+    if an > alpha then
         cases = cases + 1
-        local face1 = trangles[faceIndexes[1]]
-        verts[face1.vdx1] = {x = 0, y = 0, z = 0}
-        verts[face1.vdx2] = {x = 0, y = 0, z = 0}
-        verts[face1.vdx3] = {x = 0, y = 0, z = 0}
-
-        local face2 = trangles[faceIndexes[2]]
-        verts[face2.vdx1] = {x = 0, y = 0, z = 0}
-        verts[face2.vdx2] = {x = 0, y = 0, z = 0}
-        verts[face2.vdx3] = {x = 0, y = 0, z = 0}
+        needSmoothFace[faceIndexes[1]] = true
+        needSmoothFace[faceIndexes[2]] = true
     end
 end
 
-local output = io.open("/Users/bytedance/Desktop/ProjectX/output.obj", "w")
+local tempVerts = {}
+for faceIndex, _ in pairs(needSmoothFace) do
+    local face = trangles[faceIndex]
+    local vdx = {face.vdx1, face.vdx2, face.vdx3}
+    table.sort(
+        vdx,
+        function(a, b)
+            return a < b
+        end
+    )
+    local info1 = adjacent[vdx[1] .. "_" .. vdx[2]]
+    local info2 = adjacent[vdx[1] .. "_" .. vdx[3]]
+    local info3 = adjacent[vdx[2] .. "_" .. vdx[3]]
 
-local fmt = "v %f %f %f \n"
-for i, v in pairs(verts) do
-    output:write(string.format(fmt, v.x, v.y, v.z))
+    local idxex = {}
+    idxex[1] = faceIndex
+    idxex[2] = info1[1] ~= faceIndex and info1[1] or info1[2]
+    idxex[3] = info2[1] ~= faceIndex and info2[1] or info2[2]
+    idxex[4] = info3[1] ~= faceIndex and info3[1] or info3[2]
+    local face1 = trangles[idxex[2]]
+    local face2 = trangles[idxex[3]]
+    local face3 = trangles[idxex[4]]
+
+    local faces = {face, face1, face2, face3}
+    local tempPoints = {vdx1 = {x = 0, y = 0, z = 0}, vdx2 = {x = 0, y = 0, z = 0}, vdx3 = {x = 0, y = 0, z = 0}}
+    for i = 1, #faces do
+        for j = 1, 3 do
+            tempPoints["vdx" .. j].x = tempPoints["vdx" .. j].x + verts[faces[i]["vdx" .. j]].x * weights[idxex[i]]
+            tempPoints["vdx" .. j].y = tempPoints["vdx" .. j].y + verts[faces[i]["vdx" .. j]].y * weights[idxex[i]]
+            tempPoints["vdx" .. j].z = tempPoints["vdx" .. j].z + verts[faces[i]["vdx" .. j]].z * weights[idxex[i]]
+            
+        end
+    end
+    for j = 1, 3 do
+        tempPoints["vdx" .. j].x = tempPoints["vdx" .. j].x / 4
+        tempPoints["vdx" .. j].y = tempPoints["vdx" .. j].y / 4
+        tempPoints["vdx" .. j].z = tempPoints["vdx" .. j].z / 4
+    end
+
+    for i = 1, 3 do
+        tempVerts[face["vdx" .. i]] = {
+            x = tempPoints["vdx" .. i].x,
+            y = tempPoints["vdx" .. i].y,
+            z = tempPoints["vdx" .. i].z
+        }
+
+        verts[face["vdx" .. i]] = {
+            x = tempPoints["vdx" .. i].x,
+            y = tempPoints["vdx" .. i].y,
+            z = tempPoints["vdx" .. i].z
+        }
+    end
 end
 
-local fmt = "f %d %d %d \n"
+tempVerts = {}
+
+local output = io.open("/Users/yanwei/Desktop/ProjectX/model.obj", "w")
+
+local fmt = "v %f %f %f\n"
+for i, v in pairs(verts) do
+    local temp = tempVerts[i]
+    if temp then
+        output:write(string.format(fmt, temp.x, temp.y, temp.z))
+    else
+        output:write(string.format(fmt, v.x, v.y, v.z))
+    end
+end
+
+local fmt = "f %d %d %d\n"
 for i, tran in pairs(trangles) do
     output:write(string.format(fmt, tran.vdx1, tran.vdx2, tran.vdx3))
 end
