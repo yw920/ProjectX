0ed88d2f036232b0861389e1b9da1f8bd76640c4
yanwei.920 <yw920@icloud.com>
2020-04-14 18:33:19 +0800

    up

--- Process.sh
+++ Process.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 DIR=$(cd $(dirname $0); pwd)
 cd $DIR
-./Bin/lua main.lua $DIR/Objs/model.obj $DIR/OutPut/output.obj
+
+for ((j=1;j<=1;j++))
+do
+    if [ $j==1 ];
+    then ./Bin/lua main.lua $DIR/Objs/model.obj $DIR/OutPut/output.obj
+    else
+         ./Bin/lua main.lua $DIR/OutPut/output.obj $DIR/OutPut/output.obj
+    fi
+done
